9fb0cf4b9544843a34a345f873e1415f5a6de13c
Eddie O'Neill <ejosephoneill@gmail.com>
2020-04-08 13:00:02 -0400

    initialcommit

---
+++ CoreData-Number-Facts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-Number-Facts
+//
+//  Created by Edward O'Neill on 4/8/20.
+//  Copyright © 2020 Edward O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
